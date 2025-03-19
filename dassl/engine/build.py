fc9c23db87ed16182f68257f95f130dc483abff8
xjh <1330353477@qq.com>
2025-03-19 23:22:11 +0800

    utils文件夹完成，build.py完成

--- dassl/engine/build.py
+++ dassl/engine/build.py
@@ -4,8 +4,9 @@ TRAINER_REGISTRY = Registry("TRAINER")
 
 
 def build_trainer(cfg):
-    avai_trainers = TRAINER_REGISTRY.registered_names()
-    check_availability(cfg.TRAINER.NAME, avai_trainers)
-    if cfg.VERBOSE:
+    """根据配置中的训练器名称 (cfg.TRAINER.NAME) 构建相应的训练器。"""
+    avai_trainers = TRAINER_REGISTRY.registered_names() # 获取所有已经注册的训练器
+    check_availability(cfg.TRAINER.NAME, avai_trainers) # 检查对应名称的训练器是否存在
+    if cfg.VERBOSE: # 是否输出信息
         print("Loading trainer: {}".format(cfg.TRAINER.NAME))
-    return TRAINER_REGISTRY.get(cfg.TRAINER.NAME)(cfg)
+    return TRAINER_REGISTRY.get(cfg.TRAINER.NAME)(cfg) # 返回对应名称的训练器
--- dassl/utils/logger.py
+++ dassl/utils/logger.py
@@ -9,14 +9,11 @@ __all__ = ["Logger", "setup_logger"]
 
 
 class Logger:
-    """Write console output to external text file.
+    """将控制台输出写入外部文本文件。
+    参数:
+        fpath (str): 保存日志文件的目录。
 
-    Imported from `<https://github.com/Cysu/open-reid/blob/master/reid/utils/logging.py>`_
-
-    Args:
-        fpath (str): directory to save logging file.
-
-    Examples::
+    示例::
        >>> import sys
        >>> import os.path as osp
        >>> save_dir = 'output/experiment-1'
@@ -25,49 +22,61 @@ class Logger:
     """
 
     def __init__(self, fpath=None):
-        self.console = sys.stdout
-        self.file = None
+        self.console = sys.stdout  # 保存当前的标准输出
+        self.file = None  # 初始化文件为 None
         if fpath is not None:
-            mkdir_if_missing(osp.dirname(fpath))
-            self.file = open(fpath, "w")
+            mkdir_if_missing(osp.dirname(fpath))  # 如果目录不存在则创建
+            self.file = open(fpath, "w")  # 打开文件进行写操作
 
     def __del__(self):
-        self.close()
+        self.close()  # 在对象被销毁时关闭文件
 
     def __enter__(self):
-        pass
+        pass  # 上下文管理器的进入方法
 
     def __exit__(self, *args):
-        self.close()
+        self.close()  # 上下文管理器的退出方法，关闭文件
 
     def write(self, msg):
-        self.console.write(msg)
+        """将消息写入控制台，如果文件存在，也同时将消息写入文件。"""
+        self.console.write(msg)  # 将消息写入控制台
         if self.file is not None:
-            self.file.write(msg)
+            self.file.write(msg)  # 如果文件存在，将消息写入文件
 
     def flush(self):
-        self.console.flush()
+        """
+        强制刷新缓冲区，确保数据立即写入控制台和文件输出。
+        确保每个线程的日志及时写入文件，防止多个线程的日志互相覆盖或丢失
+        
+        相关知识：当写入文件时，Python 可能会先将数据存入缓冲区，并在适当的时候（比如关闭文件或缓冲区满了）才写入磁盘。
+        """
+        self.console.flush()  # 刷新控制台输出
         if self.file is not None:
-            self.file.flush()
-            os.fsync(self.file.fileno())
+            self.file.flush()  # 刷新文件输出
+            os.fsync(self.file.fileno())  # 确保文件内容写入磁盘
 
     def close(self):
-        self.console.close()
+        """关闭控制台和文件输出。"""
+        self.console.close()  # 关闭控制台输出
         if self.file is not None:
-            self.file.close()
+            self.file.close()  # 关闭文件
 
 
 def setup_logger(output=None):
+    """设置标准输出日志。
+    参数:
+        output (str): 日志文件的路径 (以.txt 或.log 结尾)。
+    """
     if output is None:
-        return
+        return  # 如果没有提供输出路径，则返回
 
     if output.endswith(".txt") or output.endswith(".log"):
-        fpath = output
+        fpath = output  # 如果输出路径以.txt 或.log 结尾，则直接使用该路径
     else:
-        fpath = osp.join(output, "log.txt")
+        fpath = osp.join(output, "log.txt")  # 否则，将输出路径与"log.txt"拼接
 
     if osp.exists(fpath):
-        # make sure the existing log file is not over-written
-        fpath += time.strftime("-%Y-%m-%d-%H-%M-%S")
+        # 确保现有的日志文件不会被覆盖
+        fpath += time.strftime("-%Y-%m-%d-%H-%M-%S")  # 在文件名后添加时间戳
 
-    sys.stdout = Logger(fpath)
+    sys.stdout = Logger(fpath)  # 将标准输出重定向到 Logger 实例
