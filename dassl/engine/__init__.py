e1c5fdcc12621b1491e09ae84d76879fdbc0a601
xjh <1330353477@qq.com>
2025-03-23 23:55:35 +0800

    完成了engine的精简化，完成了model的精简化

--- dassl/engine/__init__.py
+++ dassl/engine/__init__.py
@@ -1,6 +1,3 @@
 from .build import TRAINER_REGISTRY, build_trainer  # isort:skip
-from .trainer import TrainerX, TrainerXU, TrainerBase, SimpleTrainer, SimpleNet  # isort:skip
 
-from .da import *
-from .dg import *
-from .ssl import *
+from .TrainerBase import TrainerBase  # isort:skip
---
+++ dassl/model/__init__.py
@@ -0,0 +1,5 @@
+from .build import build_model, MODEL_REGISTRY  # isort:skip
+
+from .ModelBase import ModelBase # isort:skip
+
+from .SimpleModel import SimpleNet
