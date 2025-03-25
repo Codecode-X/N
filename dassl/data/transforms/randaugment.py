8ef655de58cfb1b6018f7c710a49c84be77dd3ab
Codecode-X <xiaojunhao066@gmail.com>
2025-03-25 13:34:53 +0800

    开始对transform进行工厂模式重构

--- dassl/data/transforms/randaugment.py
+++ dassl/data/transforms/randaugment.py
@@ -6,12 +6,160 @@ Credit to
 import numpy as np
 import random
 import PIL
-import torch
 import PIL.ImageOps
 import PIL.ImageDraw
 import PIL.ImageEnhance
 from PIL import Image
 
+
+class RandAugment:
+    """
+    随机数据增强 1。
+    对从 randaugment_list 中选择的 n 个操作，每个操作的强度随机选择。
+    
+    参数：
+        - n (int): 操作数量。
+        - m (int): 操作强度。
+        
+    主要功能：
+        - 对输入图像应用随机数据增强。
+        
+    主要步骤：
+        1. 从 randaugment_list 操作列表中随机选择 n 个操作。
+        2. 对每个操作，从其强度范围中随机选择一个强度值。
+        3. 对输入图像应用所选的操作。
+    """
+    def __init__(self, n=2, m=10):
+        assert 0 <= m <= 30
+        self.n = n
+        self.m = m
+        self.augment_list = [
+            (AutoContrast, 0, 1),
+            (Equalize, 0, 1),
+            (Invert, 0, 1),
+            (Rotate, 0, 30),
+            (Posterize, 4, 8),
+            (SolarizeAdd, 0, 110),
+            (Color, 0.1, 1.9),
+            (Contrast, 0.1, 1.9),
+            (Brightness, 0.1, 1.9),
+            (Sharpness, 0.1, 1.9),
+            (ShearX, 0.0, 0.3),
+            (ShearY, 0.0, 0.3),
+            (Solarize, 0, 256),
+            (CutoutAbs, 0, 40),
+            (TranslateXabs, 0.0, 100),
+            (TranslateYabs, 0.0, 100),
+        ]
+
+    def __call__(self, img):
+        ops = random.choices(self.augment_list, k=self.n)
+
+        for op, minval, maxval in ops:
+            val = (self.m / 30) * (maxval-minval) + minval
+            img = op(img, val)
+
+        return img
+
+
+class RandAugment2:
+    """
+    随机数据增强 2。
+    对从 randaugment_list2 中选择的 n 个操作，每个操作以概率 p 应用。
+
+    参数：
+        - n (int): 操作数量。
+        - p (float): 操作概率。
+
+    主要功能：
+        - 对输入图像应用随机数据增强。
+    
+    主要步骤：
+        1. 从 randaugment_list2 操作列表中随机选择 n 个操作。
+        2. 对每个操作，以概率 p 应用该操作。
+        3. 对输入图像应用所选的操作。
+    """
+
+    def __init__(self, n=2, p=0.6):
+        self.n = n
+        self.p = p
+        self.augment_list = [
+            (AutoContrast, 0, 1),
+            (Brightness, 0.1, 1.9),
+            (Color, 0.1, 1.9),
+            (Contrast, 0.1, 1.9),
+            (Equalize, 0, 1),
+            (Invert, 0, 1),
+            (Identity, 0, 1),
+            (Posterize, 4, 8),
+            (Rotate, -30, 30),
+            (Sharpness, 0.1, 1.9),
+            (ShearX, -0.3, 0.3),
+            (ShearY, -0.3, 0.3),
+            (Solarize, 0, 256),
+            (TranslateX, -0.3, 0.3),
+            (TranslateY, -0.3, 0.3),
+        ]
+
+    def __call__(self, img):
+        ops = random.choices(self.augment_list, k=self.n)
+
+        for op, minval, maxval in ops:
+            if random.random() > self.p:
+                continue
+            m = random.random()
+            val = m * (maxval-minval) + minval
+            img = op(img, val)
+
+        return img
+
+
+class RandAugmentFixMatch:
+    """
+    随机数据增强 3。
+    对从 fixmatch_list 中选择的 n 个操作，每个操作的强度随机选择。
+
+    参数：
+        - n (int): 操作数量。
+        
+    主要功能：
+        - 对输入图像应用随机数据增强。
+
+    主要步骤：
+        1. 从 fixmatch_list 操作列表中随机选择 n 个操作。
+        2. 对每个操作，从其强度范围中随机选择一个强度值。
+        3. 对输入图像应用所选的操作。
+    """
+
+    def __init__(self, n=2):
+        self.n = n
+        self.augment_list = [
+            (AutoContrast, 0, 1),
+            (Brightness, 0.05, 0.95),
+            (Color, 0.05, 0.95),
+            (Contrast, 0.05, 0.95),
+            (Equalize, 0, 1),
+            (Identity, 0, 1),
+            (Posterize, 4, 8),
+            (Rotate, -30, 30),
+            (Sharpness, 0.05, 0.95),
+            (ShearX, -0.3, 0.3),
+            (ShearY, -0.3, 0.3),
+            (Solarize, 0, 256),
+            (TranslateX, -0.3, 0.3),
+            (TranslateY, -0.3, 0.3),
+        ]
+
+    def __call__(self, img):
+        ops = random.choices(self.augment_list, k=self.n)
+
+        for op, minval, maxval in ops:
+            m = random.random()
+            val = m * (maxval-minval) + minval
+            img = op(img, val)
+
+        return img
+    
 
 def ShearX(img, v):
     assert -0.3 <= v <= 0.3
@@ -170,194 +318,5 @@ def Identity(img, v):
     return img
 
 
-class Lighting:
-    """Lighting noise (AlexNet - style PCA - based noise)."""
-
-    def __init__(self, alphastd, eigval, eigvec):
-        self.alphastd = alphastd
-        self.eigval = torch.Tensor(eigval)
-        self.eigvec = torch.Tensor(eigvec)
-
-    def __call__(self, img):
-        if self.alphastd == 0:
-            return img
-
-        alpha = img.new().resize_(3).normal_(0, self.alphastd)
-        rgb = (
-            self.eigvec.type_as(img).clone().mul(
-                alpha.view(1, 3).expand(3, 3)
-            ).mul(self.eigval.view(1, 3).expand(3, 3)).sum(1).squeeze()
-        )
-
-        return img.add(rgb.view(3, 1, 1).expand_as(img))
 
 
-class CutoutDefault:
-    """
-    Reference : https://github.com/quark0/darts/blob/master/cnn/utils.py
-    """
-
-    def __init__(self, length):
-        self.length = length
-
-    def __call__(self, img):
-        h, w = img.size(1), img.size(2)
-        mask = np.ones((h, w), np.float32)
-        y = np.random.randint(h)
-        x = np.random.randint(w)
-
-        y1 = np.clip(y - self.length // 2, 0, h)
-        y2 = np.clip(y + self.length // 2, 0, h)
-        x1 = np.clip(x - self.length // 2, 0, w)
-        x2 = np.clip(x + self.length // 2, 0, w)
-
-        mask[y1:y2, x1:x2] = 0.0
-        mask = torch.from_numpy(mask)
-        mask = mask.expand_as(img)
-        img *= mask
-        return img
-
-
-def randaugment_list():
-    # 16 oeprations and their ranges
-    # https://github.com/google-research/uda/blob/master/image/randaugment/policies.py#L57
-    # augs = [
-    #     (Identity, 0., 1.0),
-    #     (ShearX, 0., 0.3),  # 0
-    #     (ShearY, 0., 0.3),  # 1
-    #     (TranslateX, 0., 0.33),  # 2
-    #     (TranslateY, 0., 0.33),  # 3
-    #     (Rotate, 0, 30),  # 4
-    #     (AutoContrast, 0, 1),  # 5
-    #     (Invert, 0, 1),  # 6
-    #     (Equalize, 0, 1),  # 7
-    #     (Solarize, 0, 110),  # 8
-    #     (Posterize, 4, 8),  # 9
-    #     # (Contrast, 0.1, 1.9),  # 10
-    #     (Color, 0.1, 1.9),  # 11
-    #     (Brightness, 0.1, 1.9),  # 12
-    #     (Sharpness, 0.1, 1.9),  # 13
-    #     # (Cutout, 0, 0.2),  # 14
-    #     # (SamplePairing(imgs), 0, 0.4)  # 15
-    # ]
-
-    # https://github.com/tensorflow/tpu/blob/8462d083dd89489a79e3200bcc8d4063bf362186/models/official/efficientnet/autoaugment.py#L505
-    augs = [
-        (AutoContrast, 0, 1),
-        (Equalize, 0, 1),
-        (Invert, 0, 1),
-        (Rotate, 0, 30),
-        (Posterize, 4, 8),
-        (Solarize, 0, 256),
-        (SolarizeAdd, 0, 110),
-        (Color, 0.1, 1.9),
-        (Contrast, 0.1, 1.9),
-        (Brightness, 0.1, 1.9),
-        (Sharpness, 0.1, 1.9),
-        (ShearX, 0.0, 0.3),
-        (ShearY, 0.0, 0.3),
-        (CutoutAbs, 0, 40),
-        (TranslateXabs, 0.0, 100),
-        (TranslateYabs, 0.0, 100),
-    ]
-
-    return augs
-
-
-def randaugment_list2():
-    augs = [
-        (AutoContrast, 0, 1),
-        (Brightness, 0.1, 1.9),
-        (Color, 0.1, 1.9),
-        (Contrast, 0.1, 1.9),
-        (Equalize, 0, 1),
-        (Identity, 0, 1),
-        (Invert, 0, 1),
-        (Posterize, 4, 8),
-        (Rotate, -30, 30),
-        (Sharpness, 0.1, 1.9),
-        (ShearX, -0.3, 0.3),
-        (ShearY, -0.3, 0.3),
-        (Solarize, 0, 256),
-        (TranslateX, -0.3, 0.3),
-        (TranslateY, -0.3, 0.3),
-    ]
-
-    return augs
-
-
-def fixmatch_list():
-    # https://arxiv.org/abs/2001.07685
-    augs = [
-        (AutoContrast, 0, 1),
-        (Brightness, 0.05, 0.95),
-        (Color, 0.05, 0.95),
-        (Contrast, 0.05, 0.95),
-        (Equalize, 0, 1),
-        (Identity, 0, 1),
-        (Posterize, 4, 8),
-        (Rotate, -30, 30),
-        (Sharpness, 0.05, 0.95),
-        (ShearX, -0.3, 0.3),
-        (ShearY, -0.3, 0.3),
-        (Solarize, 0, 256),
-        (TranslateX, -0.3, 0.3),
-        (TranslateY, -0.3, 0.3),
-    ]
-
-    return augs
-
-
-class RandAugment:
-
-    def __init__(self, n=2, m=10):
-        assert 0 <= m <= 30
-        self.n = n
-        self.m = m
-        self.augment_list = randaugment_list()
-
-    def __call__(self, img):
-        ops = random.choices(self.augment_list, k=self.n)
-
-        for op, minval, maxval in ops:
-            val = (self.m / 30) * (maxval-minval) + minval
-            img = op(img, val)
-
-        return img
-
-
-class RandAugment2:
-
-    def __init__(self, n=2, p=0.6):
-        self.n = n
-        self.p = p
-        self.augment_list = randaugment_list2()
-
-    def __call__(self, img):
-        ops = random.choices(self.augment_list, k=self.n)
-
-        for op, minval, maxval in ops:
-            if random.random() > self.p:
-                continue
-            m = random.random()
-            val = m * (maxval-minval) + minval
-            img = op(img, val)
-
-        return img
-
-
-class RandAugmentFixMatch:
-
-    def __init__(self, n=2):
-        self.n = n
-        self.augment_list = fixmatch_list()
-
-    def __call__(self, img):
-        ops = random.choices(self.augment_list, k=self.n)
-
-        for op, minval, maxval in ops:
-            m = random.random()
-            val = m * (maxval-minval) + minval
-            img = op(img, val)
-
-        return img
